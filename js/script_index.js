c6387aa84eb011f908fcaa06b9b034ed8c2a053b
SamDeimos <zam.2014.sg@gmail.com>
2019-10-12 21:43:14 -0500

    Fix error al habilitar y desactivar grabacion de llamadas en campaña

--- js/script_index.js
+++ js/script_index.js
@@ -1,23 +1,23 @@
-function hiddenAlert(){
-	$(".alert").fadeTo(2000, 1000).slideUp(1500, function(){ 
-		$(".alert").slideUp(1500); 
+function hiddenAlert() {
+	$(".alert").fadeTo(2000, 1000).slideUp(1500, function () {
+		$(".alert").slideUp(1500);
 	});
 }
-$(function() {
+$(function () {
 	load(1);
 });
-function load(page){
-	var query=$("#q").val();
-	var per_page=10;
-	var parametros = {"action":"ajax","page":page,'query':query,'per_page':per_page};
+function load(page) {
+	var query = $("#q").val();
+	var per_page = 10;
+	var parametros = { "action": "ajax", "page": page, 'query': query, 'per_page': per_page };
 	$("#loader").fadeIn('slow');
 	$.ajax({
-		url:'src/ajax/listar_camp.php',
+		url: 'src/ajax/listar_camp.php',
 		data: parametros,
-		 beforeSend: function(objeto){
-		$("#loader").html("Cargando...");
+		beforeSend: function (objeto) {
+			$("#loader").html("Cargando...");
 		},
-		success:function(data){
+		success: function (data) {
 			$(".outer_div").html(data).fadeIn('slow');
 			$("#loader").html("");
 		}
@@ -25,31 +25,31 @@ function load(page){
 }
 $('#editProductModal').on('show.bs.modal', function (event) {
 	var button = $(event.relatedTarget) // Button that triggered the modal
-	var code = button.data('code') 
+	var code = button.data('code')
 	$('#edit_code').val(code)
-	var name = button.data('name') 
+	var name = button.data('name')
 	$('#edit_name').val(name)
-	var category = button.data('category') 
+	var category = button.data('category')
 	$('#edit_category').val(category)
-	var stock = button.data('stock') 
+	var stock = button.data('stock')
 	$('#edit_stock').val(stock)
-	var price = button.data('price') 
+	var price = button.data('price')
 	$('#edit_price').val(price)
-	var id = button.data('id') 
+	var id = button.data('id')
 	$('#edit_id').val(id)
 })
 
 $('#deleteModal').on('show.bs.modal', function (event) {
 	var button = $(event.relatedTarget) // Button that triggered the modal
-	var id = button.data('id') 
+	var id = button.data('id')
 	$('#delete_id').val(id)
-	var tabla = button.data('tabla') 
+	var tabla = button.data('tabla')
 	$('#tabla_id').val(tabla)
 })
 
 $('#pausaModal').on('show.bs.modal', function (event) {
 	var button = $(event.relatedTarget) // Button that triggered the modal
-	var id = button.data('id') 
+	var id = button.data('id')
 	$('#pausa_id').val(id)
 })
 
@@ -60,37 +60,37 @@ $('#startCampModal').on('show.bs.modal', function (event) {
 })
 
 
-$( "#edit_product" ).submit(function( event ) {
+$("#edit_product").submit(function (event) {
 	var parametros = $(this).serialize();
 	$.ajax({
-			type: "POST",
-			url: "src/ajax/editar_producto.php",
-			data: parametros,
-			 beforeSend: function(objeto){
-				$("#resultados").html("Enviando...");
-				},
-			success: function(datos){
+		type: "POST",
+		url: "src/ajax/editar_producto.php",
+		data: parametros,
+		beforeSend: function (objeto) {
+			$("#resultados").html("Enviando...");
+		},
+		success: function (datos) {
 			$("#resultados").html(datos);
 			load(1);
 			$('#editProductModal').modal('hide');
-			}
+		}
 	});
 	event.preventDefault();
 });
 
 
-$( "#add_camp" ).submit(function( event ){
+$("#add_camp").submit(function (event) {
 	var parametros = new FormData();
-	parametros.append('csvcustomFile',$('#csvcustomFile')[0].files[0]);
-	parametros.append('name',$('#name').prop('value'));
-	parametros.append('camp_h_ini',$('#camp_h_ini').prop('value'));
-	parametros.append('camp_h_fin',$('#camp_h_fin').prop('value'));
-	parametros.append('type_camp',$('#type_camp').prop('value'));
-	parametros.append('trunk',$('#trunk').prop('value'));
-	parametros.append('context',$('#context').prop('value'));
-	parametros.append('time',$('#time').prop('value'));
-	parametros.append('maxcall',$('#maxcall').prop('value'));
-	parametros.append('grabar',$('#grabar').prop('value'));
+	parametros.append('csvcustomFile', $('#csvcustomFile')[0].files[0]);
+	parametros.append('name', $('#name').prop('value'));
+	parametros.append('camp_h_ini', $('#camp_h_ini').prop('value'));
+	parametros.append('camp_h_fin', $('#camp_h_fin').prop('value'));
+	parametros.append('type_camp', $('#type_camp').prop('value'));
+	parametros.append('trunk', $('#trunk').prop('value'));
+	parametros.append('context', $('#context').prop('value'));
+	parametros.append('time', $('#time').prop('value'));
+	parametros.append('maxcall', $('#maxcall').prop('value'));
+	parametros.append('grabar', $('input:radio[name=grabar]:checked').val());
 	$.ajax({
 		type: "POST",
 		url: "src/ajax/save_camp.php",
@@ -98,65 +98,65 @@ $( "#add_camp" ).submit(function( event ){
 		processData: false,
 		contentType: false,
 		cache: false,
-		success: function(datos){
+		success: function (datos) {
 			$("#resultados").html(datos);
 			load(1);
 			$('#addCampModal').modal('hide');
-			$( "#add_camp" )[0].reset();
+			$("#add_camp")[0].reset();
 			hiddenAlert();
 		}
 	});
 	event.preventDefault();
 });
 
-$( "#delete" ).submit(function( event ) {
+$("#delete").submit(function (event) {
 	var parametros = $(this).serialize();
 	$.ajax({
-			type: "POST",
-			url: "src/ajax/delete.php",
-			data: parametros,
-			 beforeSend: function(objeto){
-				$("#resultados").html("Enviando...");
-				},
-			success: function(datos){
+		type: "POST",
+		url: "src/ajax/delete.php",
+		data: parametros,
+		beforeSend: function (objeto) {
+			$("#resultados").html("Enviando...");
+		},
+		success: function (datos) {
 			$("#resultados").html(datos);
 			load(1);
 			$('#deleteModal').modal('hide');
 			hiddenAlert();
-			}
+		}
 	});
 	event.preventDefault();
 });
 
-$( "#pausa" ).submit(function( event ) {
+$("#pausa").submit(function (event) {
 	var parametros = $(this).serialize();
 	$.ajax({
-			type: "POST",
-			url: "src/ajax/pausa_camp.php",
-			data: parametros,
-			 beforeSend: function(objeto){
-				$('#pausaModal').modal('hide');
-				},
-			success: function(datos){
+		type: "POST",
+		url: "src/ajax/pausa_camp.php",
+		data: parametros,
+		beforeSend: function (objeto) {
+			$('#pausaModal').modal('hide');
+		},
+		success: function (datos) {
 			$("#resultados").html(datos);
 			load(1);
 			$('#pausaModal').modal('hide');
 			hiddenAlert();
-			}
+		}
 	});
 	event.preventDefault();
 });
 
-$( "#startcamp" ).submit(function(event){
+$("#startcamp").submit(function (event) {
 	var parametros = $(this).serialize();
 	$.ajax({
 		type: "POST",
 		url: "src/ajax/start_camp.php",
 		data: parametros,
-		beforeSend: function(){
+		beforeSend: function () {
 			$('#startCampModal').modal('hide');
 		},
-		success: function(datos){
+		success: function (datos) {
 			$("#resultados").html(datos);
 			load(1);
 			$('#startCampModal').modal('hide');
--- src/modal/modal_add_camp.php
+++ src/modal/modal_add_camp.php
@@ -3,127 +3,127 @@ include('conexion.php');
 $troncales = listar_trunk();
 ?>
 <div id="addCampModal" class="modal fade">
-		<div class="modal-dialog modal-lg">
-			<div class="modal-content">
-				<form name="add_camp" id="add_camp">
-					<div class="modal-header">					
-						<h4 class="modal-title">Agregar Campaña</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">
-						<div class="row">
-							<div class="col-6">
-								<div class="form-group">
-									<label>Nombre de campaña</label>
-									<input type="text" name="name" id="name" class="form-control form-control-sm" required>
-								</div>
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<form name="add_camp" id="add_camp">
+				<div class="modal-header">
+					<h4 class="modal-title">Agregar Campaña</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-6">
+							<div class="form-group">
+								<label>Nombre de campaña</label>
+								<input type="text" name="name" id="name" class="form-control form-control-sm" required>
 							</div>
-							<div class="col-6">
-								<div class="form-group">
+						</div>
+						<div class="col-6">
+							<div class="form-group">
 								<label for="">Selecione una campaña</label>
 								<div class="input-group input-group-sm mb-3">
 									<div class="input-group-prepend">
-									<label class="input-group-text">Campaña</label>
+										<label class="input-group-text">Campaña</label>
 									</div>
 									<select class="form-control form-control-sm" id="type_camp" name="type_camp">
 										<option value="4010" selected="selected">Cobranza</option>
-            							<option value="4011">Marketing</option>
-            							<option value="4012">Actualización</option>
-            							<option value="1234">Prueba</option>
+										<option value="4011">Marketing</option>
+										<option value="4012">Actualización</option>
+										<option value="1234">Prueba</option>
 									</select>
-								</div>
 								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="form-group col-6">
-								<label for="">Selecione una troncal</label><!-- Troncal a usar en campaña -->
-								<div class="input-group input-group-sm mb-3">
-									<div class="input-group-prepend">
+					</div>
+					<div class="row">
+						<div class="form-group col-6">
+							<label for="">Selecione una troncal</label><!-- Troncal a usar en campaña -->
+							<div class="input-group input-group-sm mb-3">
+								<div class="input-group-prepend">
 									<label class="input-group-text">Troncal</label>
-									</div>
-									<select class="form-control form-control-sm" id="trunk" name="trunk">
-										<option value="PPM">(Plan Por Marcado)</option>
-										<?php foreach($troncales as $fila){?>
-										<option value="<?php echo strtoupper($fila['tech']) ."/" .$fila['channelid'] ?>">
-										<?php echo $fila['name'] ?>
+								</div>
+								<select class="form-control form-control-sm" id="trunk" name="trunk">
+									<option value="PPM">(Plan Por Marcado)</option>
+									<?php foreach ($troncales as $fila) { ?>
+										<option value="<?php echo strtoupper($fila['tech']) . "/" . $fila['channelid'] ?>">
+											<?php echo $fila['name'] ?>
 										</option>
-										<?php 
-										} 
-										?>
-									</select>
-								</div>
-							</div>
-							<div class="form-group col-6">
-								<label for="">Horario</label>
-								<div class="container">
-									<div class="row">
-										<input class="form-control form-control-sm col" id="camp_h_ini" name="camp_h_ini" type="time" value="00:00">
-										<p class="col-3 text-center">Hasta</p>
-										<input class="form-control form-control-sm col" id="camp_h_fin" name="camp_h_fin" type="time" value="23:59">
-									</div>
-								</div>
+									<?php
+									}
+									?>
+								</select>
 							</div>
 						</div>
-						<div class="row">
-							<div class="form-group col-6">
-								<label for="">Selecione un contexto</label><!-- Troncal a usar en campaña -->
-								<div class="input-group input-group-sm mb-3">
-									<div class="input-group-prepend">
-									<label class="input-group-text">Context</label>
-									</div>
-									<select class="form-control form-control-sm" name="context" id="context">
-										<option value="from-internal" selected="selected">from-internal</option>
-										<option value="call-file-test">call-file-test</option>
-										<option value="from-internal-custom">from-internal-custom</option>
-									</select>
-								</div>
-							</div>
-							<div class="form-group col-6">
+						<div class="form-group col-6">
+							<label for="">Horario</label>
+							<div class="container">
 								<div class="row">
-									<div class="form-group col-12">
-										<label>Duración de llamada</label>
-										<input type="text" name="time" id="time" class="form-control form-control-sm" value="35" required>
-									</div>
+									<input class="form-control form-control-sm col" id="camp_h_ini" name="camp_h_ini" type="time" value="00:00">
+									<p class="col-3 text-center">Hasta</p>
+									<input class="form-control form-control-sm col" id="camp_h_fin" name="camp_h_fin" type="time" value="23:59">
 								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="form-group col-6">
-								<label for="maxcall">Llamadas simultaneas</label>
-								<input type="text" name="maxcall" id="maxcall" class="form-control form-control-sm" value="1" required>
-							</div>
-							<div class="form-group col-6">
-								<label>Grabar llamadas</label><br>
-								<div class="form-check form-check-inline">
-									<input class="form-check-input" type="radio" value="1" name="grabar" id="grabar">
-									<label class="form-check-label" for="defaultCheck1">
-										Habilitar
-									</label>
+					</div>
+					<div class="row">
+						<div class="form-group col-6">
+							<label for="">Selecione un contexto</label><!-- Troncal a usar en campaña -->
+							<div class="input-group input-group-sm mb-3">
+								<div class="input-group-prepend">
+									<label class="input-group-text">Context</label>
 								</div>
-								<div class="form-check form-check-inline">
-									<input class="form-check-input" type="radio" value="0" name="grabar" id="grabar" checked>
-									<label class="form-check-label" for="defaultCheck1">
-										Deshabilitar
-									</label>
+								<select class="form-control form-control-sm" name="context" id="context">
+									<option value="from-internal" selected="selected">from-internal</option>
+									<option value="call-file-test">call-file-test</option>
+									<option value="from-internal-custom">from-internal-custom</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-6">
+							<div class="row">
+								<div class="form-group col-12">
+									<label>Duración de llamada</label>
+									<input type="text" name="time" id="time" class="form-control form-control-sm" value="35" required>
 								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="form-group col-12">
-								<h5>Cargar .CSV</h5>
-								<div class="custom-file">
-									<input type="file" name="csvcustomFile" id="csvcustomFile" class="custom-file-input" required>
-									<label class="custom-file-label" for="csvcustomFile" data-browse="Buscar archivo">Seleccionar Archivo</label>
-								</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-6">
+							<label for="maxcall">Llamadas simultaneas</label>
+							<input type="text" name="maxcall" id="maxcall" class="form-control form-control-sm" value="1" required>
+						</div>
+						<div class="form-group col-6">
+							<label>Grabar llamadas</label><br>
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" value="1" name="grabar" id="grabar1">
+								<label class="form-check-label" for="grabar1">
+									Habilitar
+								</label>
 							</div>
-						</div>		
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" value="0" name="grabar" id="grabar0" checked>
+								<label class="form-check-label" for="grabar0">
+									Deshabilitar
+								</label>
+							</div>
+						</div>
 					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-secondary" data-dismiss="modal" value="Cancelar">
-						<input type="submit" class="btn btn-success" value="Guardar datos">
+					<div class="row">
+						<div class="form-group col-12">
+							<h5>Cargar .CSV</h5>
+							<div class="custom-file">
+								<input type="file" name="csvcustomFile" id="csvcustomFile" class="custom-file-input" required>
+								<label class="custom-file-label" for="csvcustomFile" data-browse="Buscar archivo">Seleccionar Archivo</label>
+							</div>
+						</div>
 					</div>
-				</form>
-			</div>
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="btn btn-secondary" data-dismiss="modal" value="Cancelar">
+					<input type="submit" class="btn btn-success" value="Guardar datos">
+				</div>
+			</form>
 		</div>
 	</div>
+</div>
